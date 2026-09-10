8f06d788531304d041307a98ca009b646b0de44d
academia-currais-4bbm <sidaomelo1992@gmail.com>
2026-09-10 08:42:50 -0300

    Add files via upload

--- cloud-config.js
+++ cloud-config.js
@@ -1,5 +1,5 @@
 window.T2_CLOUD_CONFIG = {
   enabled: true,
   supabaseUrl: "https://wvedteqmfuqkwbjpiwoz.supabase.co",
-  anonKey: "sb_publishable_hicDxGPVW15LDvQGtTlJYQ_npRnwd4j"
+  publishableKey: "sb_publishable_hicDxGPVW15LDvQGtTlJYQ_npRnwd4j"
 };
